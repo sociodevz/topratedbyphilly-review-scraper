798cdc74cb454af0d92ec200cddb8359b7563987
Arun Gupta <arun@sociosquares.com>
2021-05-19 15:42:45 +0530

    houzz feature complete

--- reviews/common/config.py
+++ reviews/common/config.py
@@ -1,5 +1,8 @@
 config = {
+    'project_physical_root_path': '/var/www/html/toppicks/scraper/',
     'scraper_mode': 'online',
+    'proxy_url_ip': 'http://zxkewixn-rotate:uoqs3rcgh2ph@p.webshare.io:80',
+    'proxy_enabled': True,
 }
 
 def updateConfigFromArgs(args):
--- test.py
+++ test.py
@@ -1,20 +1,40 @@
-from reviews.main.scraper import Scraper
-import json
-from argparse import ArgumentParser
-import reviews.main
-from reviews.common.config import config, updateConfigFromArgs
+import os
+import pathlib
+import requests
+from hashlib import sha256
+from http.cookiejar import MozillaCookieJar
+from reviews.common.config import config
 
-parser = ArgumentParser()
-parser.add_argument('-engine', type=str, required=True, help='Scraping Engine [yelp,bbb,trustpilot]')
-parser.add_argument('-url', type=str, required=True, help='Website url to crawl')
-args = parser.parse_args()
+siteUrl = 'https://www.houzz.com/professionals/roofing-and-gutters/northern-lights-exteriors-pfvwus-pf~973447938'
+siteHash = sha256(siteUrl.encode('utf-8')).hexdigest()
 
-engine = args.engine.title()
-url = args.url
+cookiesFile = f"{config.get('project_physical_root_path')}tmp/cookies/{siteHash}.txt" # Places "cookies.txt" next to the script file.
+cj = MozillaCookieJar(cookiesFile)
+if os.path.exists(cookiesFile):  # Only attempt to load if the cookie file exists.
+    cj.load(ignore_discard=True, ignore_expires=True)  # Loads session cookies too (expirydate=0).
 
-if 'https://' not in url:
-    updateConfigFromArgs({f'scraper_mode=offline'})
+s = requests.Session()
+s.headers = {
+    'authority': 'www.houzz.com',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'accept': '*/*',
+    'x-requested-with': 'XMLHttpRequest',
+    'x-hz-request': 'true',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+    'x-hz-view-mode': 'null',
+    'sec-gpc': '1',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://www.houzz.com/professionals/roofing-and-gutters/northern-lights-exteriors-pfvwus-pf~973447938',
+    'accept-language': 'en-GB,en;q=0.9',
+}
 
-scraper = Scraper(engine)
-result = scraper.scrapeURL(url)
-print(json.dumps(result, indent=3))
+s.cookies = cj  # Tell Requests session to use the cookiejar.
+
+# DO STUFF HERE WHICH REQUIRES THE PERSISTENT COOKIES...
+response = s.get("https://www.houzz.com/professionals/roofing-and-gutters/northern-lights-exteriors-pfvwus-pf~973447938")
+response = s.get("https://www.houzz.com/j/ajax/profileReviewsAjax?userId=5057474&proId=177044&fromItem=12&itemsPerPage=134")
+print(response.text)
+cj.save(ignore_discard=True, ignore_expires=True)  # Saves session cookies too (expirydate=0).
