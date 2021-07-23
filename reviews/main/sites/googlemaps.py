4974a18509c6021c9b335685df2c21e79de514ca
Arun Gupta <arun@sociosquares.com>
2021-07-23 14:41:05 +0530

    headless mode chrome for server

--- reviews/main/sites/googlemaps.py
+++ reviews/main/sites/googlemaps.py
@@ -36,7 +36,7 @@ class Googlemaps:
         self.PATH = f"{config.get('project_physical_root_path')}chromedriver"
         self.options = Options()
         self.options.add_argument('--no-sandbox')
-        self.options.headless = debug
+        self.options.headless = config.get('chrome_headless_mode')
         self.browser = webdriver.Chrome(self.PATH, options=self.options)
         #self.browserReviews = webdriver.Chrome(self.PATH, options=self.options)
 
