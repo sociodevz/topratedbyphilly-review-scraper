4be66f6f74c247d82f704d183d0639745b3df807
Arun Gupta <arun@sociosquares.com>
2021-08-03 17:09:54 +0530

    yelp, toprated directory scraper

--- test.py
+++ test.py
@@ -52,44 +52,45 @@ def scrapeTopRatedDirectory(url, category):
             companyDetailsArr = []
 
             soup = BeautifulSoup(bodyHtml, 'lxml')
-            companyNameList = soup.find_all('a', attrs={'class': 'listing__heading'})
-            if companyNameList is not None:
-                for cntr, companyNameObj in enumerate(companyNameList):
-                    if cntr > 1:
+            mainWrapperDiv = soup.find('div', attrs={'id': 'w0'})
+            if mainWrapperDiv is not None:
+                companyNameList = mainWrapperDiv.find_all('a', attrs={'class': 'listing__heading'})
+                if companyNameList is not None:
+                    for cntr, companyNameObj in enumerate(companyNameList):
                         companyNameArr.append(companyNameObj.text.strip('.').split('.')[-1].strip())
                         companyUrlArr.append("https://www.topratedlocal.com" + str(companyNameObj['href']))
 
-            companyRatingList = soup.find_all('span', attrs={'class': 'pill-section'})
-            if companyRatingList is not None:
-                for companyRatingObj in companyRatingList:
-                    value = companyRatingObj.text.strip()
-                    try:
-                        float(value)
-                        companyRatingArr.append(float(value))
-                    except ValueError:
-                        pass
+                companyRatingList = mainWrapperDiv.find_all('span', attrs={'class': 'pill-section'})
+                if companyRatingList is not None:
+                    for companyRatingObj in companyRatingList:
+                        value = companyRatingObj.text.strip()
+                        try:
+                            float(value)
+                            companyRatingArr.append(float(value))
+                        except ValueError:
+                            pass
 
-            children1 = soup.find_all('div', attrs={'class': 'u-bottom5'})
-            if children1 is not None:
-                for child in children1:
-                    parentObj = child.find_parent('div')
-                    if parentObj is not None:
-                        regex = (r"Of ([0-9]*) ratings?/.*?on\n"
-                                r"              ([0-9]*) verified review sites?, this business has an average rating of\n"
-                                r"        ([0-9.]*) stars\.\n"
-                                r"    This earns them a Rating Score™ of ([0-9.]*) which ranks them #([0-9]*) in the.*?area\.")
-                        matches = re.findall(regex, bodyHtml, re.DOTALL | re.MULTILINE | re.IGNORECASE)
-                        if len(matches) > 0:
-                            for match in matches:
-                                companyDetailsArr.append(match)
+                children1 = mainWrapperDiv.find_all('div', attrs={'class': 'u-bottom5'})
+                if children1 is not None:
+                    for child in children1:
+                        parentObj = child.find_parent('div')
+                        if parentObj is not None:
+                            regex = (r"Of ([0-9]*) ratings?/.*?on\n"
+                                    r"              ([0-9]*) verified review sites?, this business has an average rating of\n"
+                                    r"        ([0-9.]*) stars\.\n"
+                                    r"    This earns them a Rating Score™ of ([0-9.]*) which ranks them #([0-9]*) in the.*?area\.")
+                            matches = re.findall(regex, bodyHtml, re.DOTALL | re.MULTILINE | re.IGNORECASE)
+                            if len(matches) > 0:
+                                for match in matches:
+                                    companyDetailsArr.append(match)
 
-            fields = ['name', 'url', 'rating', 'total_ratings', 'verified_sites', 'average_rating', 'rating_score']
-            rows = []
+                fields = ['name', 'url', 'rating', 'total_ratings', 'verified_sites', 'average_rating', 'rating_score']
+                rows = []
 
-            for (name, url, rating, detailedRating) in zip(companyNameArr, companyUrlArr, companyRatingArr, companyDetailsArr):
-                rows.append([name, url, rating, detailedRating[0], detailedRating[1], detailedRating[2], detailedRating[3]])
+                for (name, url, rating, detailedRating) in zip(companyNameArr, companyUrlArr, companyRatingArr, companyDetailsArr):
+                    rows.append([name, url, rating, detailedRating[0], detailedRating[1], detailedRating[2], detailedRating[3]])
 
-            writeCSV(f"tmp/topratedlocal_{category}.csv", fields, rows)
+                writeCSV(f"tmp/topratedlocal_{category}.csv", fields, rows)
 
 
 
@@ -160,7 +161,8 @@ def scrapeYelpDirectory(url, category):
                     pass
 
 
-# url = "https://www.topratedlocal.com/philadelphia-pa-usa/plumbers-near-me"
-# scrapeTopRatedDirectory(url, 'plumber)
-url = "https://www.yelp.com/search?find_desc=Plumbers&find_loc=Philadelphia%2C%20PA"
-scrapeYelpDirectory(url, 'plumber')
+#url = "https://www.topratedlocal.com/philadelphia-pa-usa/window-installation-near-me"
+#scrapeTopRatedDirectory(url, 'windows-installation')
+
+#url = "https://www.yelp.com/search?find_desc=Windows%20and%20Doors&find_loc=Philadelphia%2C%20PA"
+#scrapeYelpDirectory(url, 'windowsdoors')
