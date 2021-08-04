c61639df9c9fabf6ed7cf5cce96ed2fecb629b79
Arun Gupta <arun@sociosquares.com>
2021-08-04 15:43:39 +0530

    googlelocal directory scraper added

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+from reviews.common.useragents import UserAgent
 from typing import Iterable
 import requests
 import re
@@ -16,6 +17,14 @@ from reviews.common.config import config
 from reviews.common.network import Network
 from bs4 import BeautifulSoup
 from csv import writer
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 def writeCSV(fileNamePath, fields, rows):
     try:
@@ -160,9 +169,227 @@ def scrapeYelpDirectory(url, category):
                     error = e
                     pass
 
+def scrapeBuildzoomDirectory(headersArr, refererUrl, url, category):
+    headersArr['referer'] = refererUrl
+    headersArr['authority'] = 'www.buildzoom.com'
+    headersArr['path'] = url.replace('https://www.buildzoom.com', '')
+    resultArr = Network.fetch(Network.GET, url, headersArr)
+    # with open('tmp/yelpdirectory.html') as file:
+    #     resultArr = {
+    #         'code': 200,
+    #         'body': file.read()
+    #     }
+
+    if resultArr['code'] == 200:
+        bodyHtml = resultArr['body']
+
+        if bodyHtml != '':
+            companyNameArr = []
+            companyUrlArr = []
+            companyRatingArr = []
+            companyDetailsArr = []
+
+            try:
+                soup = BeautifulSoup(bodyHtml, 'lxml')
+                mainWrapperDiv = soup.find('div', attrs={'class': 'search-result-contractors'})
+                if mainWrapperDiv is not None:
+                    companyMainDivList = mainWrapperDiv.find_all('div', attrs={'class': 'search-result-contractor-secondary'})
+                    if companyMainDivList is not None:
+                        for companyMainDiv in companyMainDivList:
+                            companyNameList = companyMainDiv.find_all('div', attrs={'itemprop': 'name'})
+                            if companyNameList is not None:
+                                for companyName in companyNameList:
+                                    companyNameArr.append(companyName.text.strip())
+
+                            companyUrlList = companyMainDiv.find_all('a', attrs={'itemprop': 'url'})
+                            if companyUrlList is not None:
+                                for companyUrl in companyUrlList:
+                                    companyUrlArr.append('https://www.buildzoom.com' + companyUrl['href'].strip())
+
+                            companyRating = 0
+                            companyRatingList = companyMainDiv.find_all('span', attrs={'itemprop': 'ratingValue'})
+                            if companyRatingList is not None and len(companyRatingList) > 0:
+                                for companyRating in companyRatingList:
+                                    companyRating = companyRating.text.strip()
+                            else:
+                                companyRatingListOuter = companyMainDiv.find('div', attrs={'class': 'contractor-reviews'})
+                                if companyRatingListOuter is not None:
+                                    spanRatingObj = companyRatingListOuter.find('span', attrs={'class': 'hidden'})
+                                    if spanRatingObj is not None:
+                                        companyRating = spanRatingObj.text.strip()
+
+                            companyRatingArr.append(companyRating)
+
+                            companyReviewsCount = 0
+                            companyRatingCountList = companyMainDiv.find_all('span', attrs={'itemprop': 'reviewCount'})
+                            if companyRatingCountList is not None and len(companyRatingCountList) > 0:
+                                for companyRatingCount in companyRatingCountList:
+                                    companyReviewsCount = companyRatingCount.text.strip()
+                            else:
+                                companyReviewsListOuter = companyMainDiv.find('div', attrs={'class': 'contractor-reviews'})
+                                if companyReviewsListOuter is not None:
+                                    aHrefObj = companyReviewsListOuter.find('a')
+                                    if aHrefObj is not None:
+                                        spanReviewCountObj = aHrefObj.find('span')
+                                        if spanReviewCountObj is not None:
+                                            companyReviewsCount = spanReviewCountObj.text.strip()
+
+                            companyDetailsArr.append(companyReviewsCount)
+
+                        fields = ['name', 'url', 'rating', 'total_ratings']
+                        rows = []
+
+                        for (name, landingUrl, rating, totalRatings) in zip(companyNameArr, companyUrlArr, companyRatingArr, companyDetailsArr):
+                            rows.append([name, landingUrl, rating, totalRatings])
+
+                        ts = datetime.datetime.now().timestamp()
+                        writeCSV(f"tmp/buildzoom_{category}.csv", fields, rows)
+
+                        #lets try and extract next page url
+                        nextPageUrlElement = soup.find('a', attrs={'rel': 'next'})
+                        if nextPageUrlElement is not None:
+                            nextUrl = 'https://www.buildzoom.com' + nextPageUrlElement['href']
+                            time.sleep(3)
+                            scrapeBuildzoomDirectory(headersArr, url, nextUrl, category)
+            except Exception as e:
+                error = e
+                print(e)
+                pass
+
+def scrapeBBBDirectory(headersArr, refererUrl, url, category):
+    headersArr['referer'] = refererUrl
+    headersArr['authority'] = 'www.bbb.org'
+    headersArr['path'] = url.replace('https://www.bbb.org', '')
+    resultArr = Network.fetch(Network.GET, url, headersArr)
+    # with open('tmp/yelpdirectory.html') as file:
+    #     resultArr = {
+    #         'code': 200,
+    #         'body': file.read()
+    #     }
+
+    if resultArr['code'] == 200:
+        jsonStr = resultArr['body']
+        jsonArr = json.loads(jsonStr)
+
+        if len(jsonArr) > 0:
+            companyNameArr = []
+            companyUrlArr = []
+            companyRatingArr = []
+            companyDetailsArr = []
+
+            try:
+                for result in jsonArr['results']:
+                    businessName = result['businessName'].strip()
+                    companyNameArr.append(result['businessName'].strip().replace('<em>', '').replace('</em>', ''))
+                    companyUrlArr.append(result['reportUrl'].strip())
+                    companyRatingArr.append(result['rating'])
+                    companyDetailsArr.append(result['score'])
+
+                fields = ['name', 'url', 'rating', 'rating_score']
+                rows = []
+
+                for (name, landingUrl, rating, totalRatings) in zip(companyNameArr, companyUrlArr, companyRatingArr, companyDetailsArr):
+                    rows.append([name, landingUrl, rating, totalRatings])
+
+                ts = datetime.datetime.now().timestamp()
+                writeCSV(f"tmp/bbb_{category}.csv", fields, rows)
+
+                #lets try and extract next page url
+                currentPageNum = jsonArr['page']
+                totalPages = jsonArr['totalPages']
+                if currentPageNum < totalPages:
+                    regex = r"&page=(.*?)&"
+                    nextPageNumber = currentPageNum + 1
+                    subst = f"&page={nextPageNumber}&"
+                    nextUrl = re.sub(regex, subst, url, 0, re.MULTILINE)
+                    time.sleep(3)
+                    scrapeBBBDirectory(headersArr, url.replace('api/', ''), nextUrl, category)
+            except Exception as e:
+                error = e
+                print(e)
+                pass
+
+def scrapeGoogleDirectory(headersArr, refererUrl, url, category):
+    path = f"{config.get('project_physical_root_path')}chromedriver"
+    options = Options()
+    options.add_argument('--no-sandbox')
+    options.headless = config.get('chrome_headless_mode')
+    browser = webdriver.Chrome(path, options=options)
+    try:
+        browser.get(url)
+        time.sleep(5)
+        bodyHtml = str(browser.page_source).encode('utf8').decode('unicode_escape')
+        bodyHtml = bodyHtml.replace("\\u0026","&").replace("\\u003d","=").replace("\\n",'\n')
+        browser.quit()
+
+        # with open('tmp/googledirectory.html') as file:
+        #         resultArr = {
+        #             'code': 200,
+        #             'body': file.read()
+        #         }
+        # bodyHtml = resultArr['body']
+
+        soup = BeautifulSoup(bodyHtml, 'lxml')
+        if soup is not None:
+            # save memory quit browser
+            companyNameArr = []
+            companyUrlArr = []
+            companyRatingArr = []
+            companyDetailsArr = []
+
+            regex = r'\["(https://www.google.com/localservices/provider\?cid=.*?)",null,null,"(.*?)\\nPlumber\\nStar rating: (.*?)"'
+            matches = re.findall(regex, bodyHtml)
+            if len(matches) > 0:
+                for match in matches:
+                    companyNameArr.append(match[1])
+                    companyUrlArr.append(match[0])
+                    companyRatingArr.append(match[2])
+
+            reviewCountOuterList = soup.findAll('div', attrs={'class': 'zFYXkc'})
+            if reviewCountOuterList is not None:
+                for reviewCountOuterObj in reviewCountOuterList:
+                    reviewCountList = reviewCountOuterObj.findAll('span', attrs={'class': 'rBK1zb'})
+                    if reviewCountList is not None:
+                        for reviewCountObj in reviewCountList:
+                            reviewCountSpanList = reviewCountObj.findAll('span')
+                            for reviewCountSpanObj in reviewCountSpanList:
+                                value = reviewCountSpanObj.text.strip()
+                                try:
+                                    int(value)
+                                    companyDetailsArr.append(float(value))
+                                    continue
+                                except ValueError:
+                                    pass
+
+            fields = ['name', 'url', 'rating', 'total_ratings']
+            rows = []
+
+            for (name, landingUrl, rating, totalRatings) in zip(companyNameArr, companyUrlArr, companyRatingArr, companyDetailsArr):
+                rows.append([name, landingUrl, rating, totalRatings])
+
+            ts = datetime.datetime.now().timestamp()
+            writeCSV(f"tmp/googlelocal_{category}.csv", fields, rows)
+
+
+    except Exception as e:
+        print(e)
+        browser.quit()
+
+
+userAgent = UserAgent()
+headersArr = userAgent.getRandom()
 
 #url = "https://www.topratedlocal.com/philadelphia-pa-usa/window-installation-near-me"
-#scrapeTopRatedDirectory(url, 'windows-installation')
+#scrapeTopRatedDirectory(headersArr, 'https://www.topratedlocal.com', url, 'windows-installation')
 
 #url = "https://www.yelp.com/search?find_desc=Windows%20and%20Doors&find_loc=Philadelphia%2C%20PA"
-#scrapeYelpDirectory(url, 'windowsdoors')
+#scrapeYelpDirectory(headersArr, 'https://www.yelp.com', url, 'windowsdoors')
+
+#url = "https://www.buildzoom.com/philadelphia-pa/windows-and-doors"
+#scrapeBuildzoomDirectory(headersArr, 'https://www.buildzoom.com/', url, 'windows-and-doors')
+
+#url = "https://www.bbb.org/api/search?find_country=USA&find_latlng=39.989654%2C-75.148976&find_loc=Philadelphia%2C%20PA&find_text=windows%20and%20doors&page=1&sort=Distance"
+#scrapeBBBDirectory(headersArr, 'https://www.bbb.org', url, 'windows-doors')
+
+url = "https://www.google.com/search?tbm=lcl&q=windows+and+doors+replacement+companies+in+philadelphia&spell=1&sa=X&ved=2ahUKEwiUsIfJ3ZTyAhXRyIsBHe4JDsoQBSgAegQIAxAm&biw=1366&bih=657&dpr=1#rlfi=hd:;si:;mv:[[40.1328331,-74.9924552],[39.902724299999996,-75.18228760000001]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:14"
+scrapeGoogleDirectory(headersArr, 'https://www.google.com/', url, 'windows-doors')
