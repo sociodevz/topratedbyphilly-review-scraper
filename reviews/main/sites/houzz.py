35dcf6b64e97123a6f6ffb0cfb3f11910ad134ff
Arun Gupta <arun@sociosquares.com>
2021-08-12 14:07:56 +0530

    image scraping added

--- reviews/main/sites/houzz.py
+++ reviews/main/sites/houzz.py
@@ -7,13 +7,29 @@ import re
 import os
 import json
 import math
+import time
+import wget
+import traceback
+
 from random import randint, random, randrange
 from time import sleep
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+
 from reviews.common.network import Network
 from reviews.common.config import config
 from reviews.main.reviews_formatter import ReviewFormatter
 from reviews.common.functions import *
 from reviews.common.logger import logger
+
 
 class Houzz:
 
@@ -29,7 +45,66 @@ class Houzz:
         logger.info(f'Initalized {self.platformName} Engine')
         pass
 
-    def scrapeURL(self, url):
+    def scrapeListings(self, url, csvFileNamePath):
+        return 'Not yet implemented'
+
+    def scrapeImages(self, url, imageSavePath):
+        PATH = f"{config.get('project_physical_root_path')}chromedriver"
+        options = Options()
+        options.add_argument('--no-sandbox')
+        options.headless = config.get('chrome_headless_mode')
+
+        urlsList = [url]  # future provision for multiple cli urls
+
+        for url in urlsList:
+            browser = webdriver.Chrome(PATH, options=options)
+
+            try:
+                originalUrl = url
+                browser.get(originalUrl)
+                time.sleep(5)
+                val = input("Continue:")
+
+                websiteName = None
+                soup = BeautifulSoup(browser.page_source, 'lxml')
+                websiteNameElement = soup.find('meta', attrs={'name': 'author'})
+                websiteName = websiteNameElement['content'].strip()
+
+                projects = soup.findAll('a', attrs={'data-testid': 'image-card-link'})
+                if projects is not None:
+                    for project in projects:
+                        browser1 = webdriver.Chrome(PATH, options=options)
+                        browser1.get(project['href'])
+                        time.sleep(5)
+
+                        imageArr = []
+                        soup = BeautifulSoup(browser1.page_source, 'lxml')
+                        projectNameElement = soup.find('h1', attrs={'class': 'header-1'})
+                        if projectNameElement is not None:
+                            projectName = projectNameElement.text.strip()
+                            projectNamePath = f"{imageSavePath}/{websiteName}/{projectName}"
+                            Path(projectNamePath).mkdir(parents=True, exist_ok=True)
+
+                            images = soup.findAll('source')
+                            if images is not None:
+                                for image in images:
+                                    imageUrl = image['srcset'].replace('w378', 'w1024').replace('h378', 'h1024')
+                                    try:
+                                        image_filename = wget.download(imageUrl, out=projectNamePath)
+                                    except Exception as e:
+                                        pass
+                        browser1.quit()
+                browser.quit()
+            except Exception as e:
+                tb = traceback.format_exc()
+                print(tb)
+                error = e
+                print(error)
+                # browser.quit()
+                # browser1.quit()
+                pass
+
+    def scrapeReviews(self, url):
         returnArr = []
 
         self.siteUrl = url
